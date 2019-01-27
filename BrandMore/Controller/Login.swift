89af79a968935d7730b5c82bb8016f343bc61b33
DhrubojyotiBis1 <bisdhruv1998@gmail.com>
2019-01-27 21:54:20 +0530

    add login Page

---
+++ BrandMore/Controller/Login.swift
@@ -0,0 +1,64 @@
+//
+//  Login.swift
+//  BrandMore
+//
+//  Created by Dhrubojyoti on 27/01/19.
+//  Copyright © 2019 Dhrubojyoti. All rights reserved.
+//
+
+import UIKit
+
+class Login: UIViewController,UITextFieldDelegate {
+
+    //MARK:- Global variable
+    @IBOutlet var outSide: UIView!
+    @IBOutlet weak var passwordTextView: UITextField!
+    @IBOutlet weak var emailTextView: UITextField!
+    @IBOutlet weak var createNewAccountButton: UIButton!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var whiteBackground: UIView!
+    
+    //MARK:- Initial
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        //TODO: Delegate here:
+        passwordTextView.delegate = self
+        emailTextView.delegate = self
+        
+        //TODO: gesture registration and creation here:
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(tappedOutSide))
+        outSide.addGestureRecognizer(tapGesture)
+        
+        //TODO: Function here:
+        makeRoundedCorner()
+    }
+    
+    
+    
+    //MARK:- Button
+    @IBAction func createNewAccountButtonPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func loginButtonPressed(_ sender: Any) {
+    }
+    
+    
+    
+    //MARK:- Function
+    @objc func tappedOutSide(){
+        passwordTextView.endEditing(true)
+        emailTextView.endEditing(true)
+    }
+    
+    func makeRoundedCorner(){
+        //Function that makes the corner of the button and the white background of the page round with a given radiuus
+        createNewAccountButton.layer.cornerRadius = 10
+        loginButton.layer.cornerRadius = 10
+        whiteBackground.layer.cornerRadius = 20
+    }
+
+}
--- BrandMore/Controller/ViewController.swift
+++ BrandMore/Controller/ViewController.swift
@@ -8,17 +8,63 @@
 
 import UIKit
 
-class LogIn: UIViewController {
+class CreateNewUser: UIViewController,UITextFieldDelegate {
 
+
+    //MARK:- Global Variable:
+    @IBOutlet var outSide: UIView!
+    @IBOutlet weak var conformPasswordTextView: UITextField!
+    @IBOutlet weak var passwordTextView: UITextField!
+    @IBOutlet weak var pinTextView: UITextField!
+    @IBOutlet weak var emailTextVlew: UITextField!
+    @IBOutlet weak var nameTextView: UITextField!
     @IBOutlet weak var loginButton: UIButton!
     @IBOutlet weak var loginWhiteBackground: UIView!
     @IBOutlet weak var createAcountButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //MARK:- Initial
+        //TODO: All delegate here:
+        
+        conformPasswordTextView.delegate = self
+        passwordTextView.delegate = self
+        nameTextView.delegate = self
+        emailTextVlew.delegate = self
+        pinTextView.delegate = self
+        
+        //TODO:gesture registration and creation here:
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(tappedOutSide))
+        outSide.addGestureRecognizer(tapGesture)
+        
+        
+        
+        //TODO: Function here:
         makeRoundCorner()
     }
     
+    
+    //MARK:- Buttons:
+    @IBAction func loginButtonPressed(_ sender: UIButton) {
+        performSegue(withIdentifier: "goToLogin", sender: sender)
+    }
+    
+    
+    @IBAction func createNewAccountButtonPressed(_ sender: Any) {
+    }
+    
+    
+    
+    //MARK:- Function:
+    @objc func tappedOutSide(){
+        conformPasswordTextView.endEditing(true)
+        passwordTextView.endEditing(true)
+        nameTextView.endEditing(true)
+        emailTextVlew.endEditing(true)
+        pinTextView.endEditing(true)
+    }
     
     func makeRoundCorner(){
     //Function that makes the corner of the button and the white background of the page round with a given radiuus
